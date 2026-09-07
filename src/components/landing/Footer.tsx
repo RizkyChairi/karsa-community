949a707c8d261219d6b8371dce715fa0b452f79a
Jona Al Farros <124805962+MetamorphosisDev@users.noreply.github.com>
2026-09-07 14:32:26 +0700

    feat: add setting background typograph and layout navbar

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -15,3 +15,25 @@ const information = [
   ["Program", "/kegiatan"],
   ["Dokumentasi", "/galeri"],
 ]
+
+export default function Footer() {
+  return (
+    <footer className="relative overflow-hidden bg-karsa-black text-white">
+
+      {/* Background Typography */}
+      <div
+        aria-hidden="true"
+        className="pointer-events-none absolute inset-x-0 bottom-[-1rem] select-none overflow-hidden"
+      >
+        <p className="translate-y-5 whitespace-nowrap text-center text-[23vw] font-black leading-none tracking-[-0.09em] text-white/[0.035]">
+          KARSA
+        </p>
+      </div>
+
+      <div className="relative z-10 mx-auto max-w-7xl px-6 pt-14 md:px-10 md:pt-20">
+
+
+      </div>
+    </footer>
+  )
+}
